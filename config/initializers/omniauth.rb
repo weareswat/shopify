591df3ff55e383bd33de4794fd8e33f268ea6ed4
Pedro Sousa <froidexplica@gmail.com>
2012-11-30 13:33:02 +0000

    new

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,14 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :shopify, 
+           ShopifyApp.configuration.api_key, 
+           ShopifyApp.configuration.secret,
+
+           # Example permission scopes - see http://api.shopify.com/authentication.html for full listing
+           # :scope => 'read_orders, write_products',
+           :scope => "write_products,read_orders",
+           :setup => lambda {|env| 
+                       params = Rack::Utils.parse_query(env['QUERY_STRING'])
+                       site_url = "https://#{params['shop']}"
+                       env['omniauth.strategy'].options[:client_options][:site] = site_url
+                     }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invoicexpress::Application.config.secret_token = 'b10d64ac67f6cc6c8de5c47ed1a258dec0acbaf3e03542d06099bd6db2fad091490387ecd4e71c76e1006249b10ae1c85ab6f3b8461a67f08c656f45b103480b'
